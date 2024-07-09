1c1e0a51568aacb4476d02bc4521126438d4aff4
Caitlan Tirrell <CaitlanTirrell@gmail.com>
2024-07-09 14:46:43 -0600

    Create logic for asking for multiple responses

--- index.js
+++ index.js
@@ -7,12 +7,30 @@ var rs = require('readline-sync');
 //store list of users hobbies
 //print user hobby list at the end
 
-var hobbies = [];
+const hobbies = [];
 
-//var userName = rs.question('What is your name? ');
-var favHobby = rs.question('What is your favorite hobby? ');
+var userName = rs.question('What is your name? ');
+var favHobby = rs.question(userName + ', what is your favorite hobby? ');
+
 hobbies.push(favHobby);
 
+var moreHobbies = rs.questionInt('How many other hobbies do you have? ')
 
 
-console.log(hobbies);
+if(moreHobbies > 0){
+    var counter = 0;
+    while(counter < moreHobbies){
+        var input = rs.prompt();
+        hobbies.push(input);
+        counter++;
+
+        if(counter < moreHobbies){
+            console.log('-- Awesome! "' + input + '" is cool! What\'s the other ' + (moreHobbies - counter) + '?');
+        }else{
+            console.log('I wish I could do ' + hobbies + '...but I\'m not even real');
+        }
+    }
+}else{
+    var areYouSure = rs.keyInYN('Are you sure? ' +  hobbies[0] + ' is all you like to do?');
+}
+//console.log(hobbies);

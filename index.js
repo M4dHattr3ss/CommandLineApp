836e2eb74300dcc46f401af98d1d03bd32268a8e
Caitlan Tirrell <CaitlanTirrell@gmail.com>
2024-07-09 15:29:36 -0600

    Clean up and create functions

--- index.js
+++ index.js
@@ -8,29 +8,45 @@ var rs = require('readline-sync');
 //print user hobby list at the end
 
 const hobbies = [];
+var areYouSure = false;
+var moreHobbies = 0;
+
+function getHobbyCount(){
+    moreHobbies = rs.questionInt('How many other hobbies do you have? ');
+}
+
+function addHobbies(numHobbies){
+    var counter = 0;
+    while(counter < numHobbies){
+        var input = rs.prompt();
+        hobbies.push(input);
+        counter++;
+
+        if(counter < numHobbies){
+            console.log('-- Awesome! "' + input + '" is cool! What\'s the other ' + (numHobbies - counter) + '?');
+        }else{
+            console.log('I wish I could do ' + hobbies + '...but I\'m not even real');
+        }
+    }
+}
 
 var userName = rs.question('What is your name? ');
 var favHobby = rs.question(userName + ', what is your favorite hobby? ');
 
 hobbies.push(favHobby);
-
-var moreHobbies = rs.questionInt('How many other hobbies do you have? ')
-
+getHobbyCount();
 
 if(moreHobbies > 0){
-    var counter = 0;
-    while(counter < moreHobbies){
-        var input = rs.prompt();
-        hobbies.push(input);
-        counter++;
-
-        if(counter < moreHobbies){
-            console.log('-- Awesome! "' + input + '" is cool! What\'s the other ' + (moreHobbies - counter) + '?');
-        }else{
-            console.log('I wish I could do ' + hobbies + '...but I\'m not even real');
-        }
-    }
+    console.log('Cool, what are they? ');
+    addHobbies(moreHobbies);
 }else{
-    var areYouSure = rs.keyInYN('Are you sure? ' +  hobbies[0] + ' is all you like to do?');
+    areYouSure = rs.keyInYN('Are you sure? ' +  hobbies[0] + ' is all you like to do?');
 }
-//console.log(hobbies);
+
+if(!areYouSure && hobbies.length <= 1){
+    getHobbyCount();
+    console.log('Cool, what are they? ');
+    addHobbies(moreHobbies);
+}else if(areYouSure){
+    console.log(hobbies + ' is cool, good for you.');
+}

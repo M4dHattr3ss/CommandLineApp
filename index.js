f39e4f182b13ec1e096b08a22b247b18e188fb29
Caitlan Tirrell <CaitlanTirrell@gmail.com>
2024-07-09 14:15:44 -0600

    Create array for storing hobbies

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 var rs = require('readline-sync');
 
-var userName = rs.question('What is your name? ');
-console.log('Hi ' + userName + '!');
+//prompt the name question
+//prompt the fav hobby
+//ask if there's more hobbies
+//add hobbies to a list
+//store list of users hobbies
+//print user hobby list at the end
+
+var hobbies = [];
+
+//var userName = rs.question('What is your name? ');
+var favHobby = rs.question('What is your favorite hobby? ');
+hobbies.push(favHobby);
+
+
+
+console.log(hobbies);
